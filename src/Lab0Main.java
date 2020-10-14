ced0eac9bb796d312daa1d023320192c7faee652
João Bandeira <undisclosed>
2020-10-14 15:11:14 +0100

    Alteração

--- src/Lab0Main.java
+++ src/Lab0Main.java
@@ -3,6 +3,7 @@ import com.pa.Person;
 
 public class Lab0Main {
     public static void main(String[] args) {
+
         Person person = new Person(1, "João");
         Person person2 = new Person(2, "António");
         System.out.println(person2.toString());
@@ -10,5 +11,6 @@ public class Lab0Main {
         Group group = new Group(person);
         group.addMember(person2);
         System.out.println(group.toString());
+
     }
 }
--- src/test/PersonTest.java
+++ src/test/PersonTest.java
@@ -12,4 +12,25 @@ public class PersonTest {
         Person p1 = new Person(1, "aaaa");
         assertEquals("aaaa" , p1.getName());
     }
+
+    @Test
+    public void testGetID(){
+        Person p1 = new Person(1, "João");
+        assertEquals(1, p1.getId());
+    }
+
+    @Test
+    public void testSetID(){
+        Person p1 = new Person(4 , "António");
+        p1.setId(2);
+        assertEquals(2, p1.getId());
+
+    }
+
+    @Test
+    public void testSetName(){
+        Person p1 = new Person(3 , "Zé");
+        p1.setName("José");
+        assertEquals("José", p1.getName());
+    }
 }
