79d54a5a1b7a682517b3034ca400827264566dd1
João Bandeira <undisclosed>
2020-10-14 14:55:27 +0100

    Alteração

---
+++ src/Lab0Main.java
@@ -0,0 +1,14 @@
+import com.pa.Group;
+import com.pa.Person;
+
+public class Lab0Main {
+    public static void main(String[] args) {
+        Person person = new Person(1, "João");
+        Person person2 = new Person(2, "António");
+        System.out.println(person2.toString());
+
+        Group group = new Group(person);
+        group.addMember(person2);
+        System.out.println(group.toString());
+    }
+}
